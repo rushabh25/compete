8d9649b120a72afc0d9292467db35617248aeac0
Rushabh Shah <rshah@comscore.com>
2017-03-29 17:22:24 -0400

    udpated the main file to include few more filters

--- sortable/src/main/scala/Main.scala
+++ sortable/src/main/scala/Main.scala
@@ -31,6 +31,8 @@ object Main {
       appName("sortable-test").
       getOrCreate()
 
+    import spark.implicits._
+
     val productsDSRaw = spark.read.json("maprfs:///mapr/ri0.comscore.com/user/rshah/learning/products.txt").
       withColumnRenamed(existingName = "announced-date", newName = "announced_date").as[Products]
 
@@ -58,11 +60,23 @@ object Main {
     // first layer of filtering where atleast manufacturer name should match
     // or manufacturer name of the product should occur in the listing title
     val filtered = cartesian.
-      filter(col("a.manufacturer").contains(col("b.manufacturer")) ||
-        col("b.manufacturer").contains(col("a.manufacturer")) ||
-        col("b.title").contains(col("a.manufacturer"))
+      filter($"a.manufacturer".contains($"b.manufacturer") ||
+        $"b.manufacturer".contains($"a.manufacturer") ||
+        $"b.title".contains($"a.manufacturer")
       )
+    // count: 1198704
 
+    // remove words like for, fur
+    // e.g. kameratasche modell hardcase, dunkelblau, im set mit 4 gb sd-card für sony dsc-w570, w530, w510, wx10, wx10, w310, 320, 350, 380, tx7, 10, t99, 110]
+
+    val anotherFilter = filtered.filter(!$"b.title".contains(" für ") &&
+      !($"b.title".contains(" for ")) &&
+      !($"b.title".contains(" pour ")))
+    // count: 1154491
+
+    // split the model column on all delimiters like ' ', '-' etc
+    // then see if either of them matches the title column
+    val delimitedFilter = anotherFilter.withColumn("split_col", split($"model", "[\\-\\s]"))
 
 
 
